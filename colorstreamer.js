02cf14fd4fcc2b8a8d925aee2a7b4ad2f39ddf69
mknadler <kennedy.nadler@gmail.com>
2014-12-23 16:52:14 -0500

    Add deletion of >max colors

--- colorstreamer.js
+++ colorstreamer.js
@@ -5,25 +5,36 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     colors: function() {
-      return Colors.find({});
+      return Colors.find({}, {sort: {createdAt: -1}});
     }
   });
 
   Template.body.events({
     "submit .new-color": function( event ){
       var color = event.target.color.value;
-
       Colors.insert({
         color: color,
         createdAt: new Date()
       });
 
-      event.target.color.value = "#FFFFFF";
+      event.target.color.value = "";
 
+      //console.log(allColors.length);
+      // remove anything in Colors besides the last 10
+      var toDelete = Colors.find({}, {sort: {createdAt: -1}, skip: 10});
+      toDelete.forEach(function (color){
+        Colors.remove(color._id);
+      });
       return false;
 
     }
   });
+
+  Template.colorbar.events({
+    "click .colorbar": function () {
+          Colors.remove(this._id);
+        }
+});
 
 }
 
