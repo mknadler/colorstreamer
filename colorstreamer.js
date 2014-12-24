9b367f519daaed2cb5e44d885e7282c05b11a171
mknadler <kennedy.nadler@gmail.com>
2014-12-24 17:09:08 -0500

    Insecure new color picker

--- colorstreamer.js
+++ colorstreamer.js
@@ -1,11 +1,16 @@
 Colors = new Mongo.Collection("colors");
+ColorOptions = new Mongo.Collection("options")
 
 if (Meteor.isClient) {
-
 
   Template.body.helpers({
     colors: function() {
       return Colors.find({}, {sort: {createdAt: -1}});
+    },
+    option: function() {
+      var result = ColorOptions.find();
+      console.log(result);
+      return ColorOptions.find();
     }
   });
 
@@ -14,10 +19,18 @@ if (Meteor.isClient) {
       var color = event.target.color.value;
       Meteor.call("addColor", color);
       event.target.color.value = "";
-
       event.preventDefault();
-
       return false;
+    }
+  });
+
+  Template.colorpick.helpers({
+  });
+
+  Template.colorpick.events({
+    "click div.color-option": function(){
+      console.log(this.color);
+      Meteor.call("addColor", this.color);
     }
   });
 
@@ -25,7 +38,7 @@ if (Meteor.isClient) {
     "click .colorbar": function () {
           return false;
         }
-});
+  });
 
 }
 
@@ -39,12 +52,19 @@ Meteor.methods({
       toDelete.forEach(function (color){
         Colors.remove(color._id);
       });
-  },
- 
+  }
+
 });
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (ColorOptions.find().count() === 0) {
+      var options = ["#c1272d", "#ed1c24", "#f15a24","#f7931e","#fbb03b","#fcee21","#d9e021","#8cc63f","#39b54a","#009245","#006837","#22b573","#00a99d","#29abe2","#0071bc","#2e3192","#1b1464","#662d91","#93278f","#9e005d","#d4145a","#ed1e79","#000000","#333333","#808080","#CCCCCC","#F2F2F2"];
+      _.each(options, function(col){
+        ColorOptions.insert({
+          color: col
+        });
+      });
+    }
   });
 }
